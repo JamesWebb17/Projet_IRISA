9592dd2d79cf75e75855956c3cbe80a6249a45af
Faucher Simon <sfaucher@enssat.fr>
2023-10-20 14:18:55 +0200

    ajout valeur de temps

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Exemple d'utilisation
+
 from Lecture_Fichier.stat import Stat
 from Lecture_Fichier.uptime import Uptime
+import time
 
 
 def calcul_utilisation_cpu(stat, uptime, clock_ticks_per_second):
@@ -17,13 +19,26 @@ def calcul_utilisation_cpu(stat, uptime, clock_ticks_per_second):
 
 
 def main():
-   pid = 21863  # Remplacez par le PID du processus que vous souhaitez inspecter
-   process_info = Stat(pid)
-   process_info.read_proc_stat(pid)
-   process_info.display_info()
-   uptime_info = Uptime()
-   uptime_info.read_proc_uptime()
-   print(f"Utilisation du CPU : {calcul_utilisation_cpu(process_info,uptime_info,100)} %")
+    pid = 21863  # Remplacez par le PID du processus que vous souhaitez inspecter
+    process_info = Stat(pid)
+    process_info.read_proc_stat(pid)
+    process_info.display_info()
+    uptime_info = Uptime()
+    uptime_info.read_proc_uptime()
+    print(f"Utilisation du CPU : {} %")
+
+    Liste_CPU = []
+    compt = 0
+    while compt < 100:
+        current_time = time.localtime()
+
+        # Formattez l'heure comme une chaîne de caractères
+        time_str = time.strftime("%H:%M:%S", current_time)
+
+        Liste_CPU.append([calcul_utilisation_cpu(process_info,uptime_info,100),time_str])
+        compt += 1
+    print(Liste_CPU)
+
 
 
 if __name__ == "__main__":
