49f65493a9f2ffea95b89b06c05104e6a7640cbd
Faucher Simon <sfaucher@enssat.fr>
2023-11-09 17:12:23 +0100

    Application des corrections + commentaire à MEM

--- Memory/utilisation.py
+++ Memory/utilisation.py
@@ -1,60 +1,43 @@
-import matplotlib.pyplot as plt
+""" @package Memory
+Documentation for Memory module.
+
+More details.
+Function for calculating the Memory usage of a process.
+"""
+
 import time
 
 from Read_File import Statm, Uptime
-from shared import locking, Result
+from shared import Result
 
 
-def calcul_utilisation_mem(statm, uptime, clock_ticks_per_second):
-    process_size = statm.size
-    process_resident = statm.resident
-    process_share = statm.share
-    process_start_sec = statm.starttime / clock_ticks_per_second
+def utilisation_mem(pid, frequency, interval, result):
+    """
+    Find the Memory usage of a process in files /proc/[pid]/statm and /proc/uptime.
+    :param pid: pid of the process
+    :param frequency: frequency of the points
+    :param interval: interval of time wanted
+    :param result: array for sending the result to the main thread
+    :return: status of the function
+    """
 
-    system_uptime_sec = uptime.total_time
-
-    process_elapsed_sec = system_uptime_sec - process_start_sec
-    process_mem_usage = 100 * ((process_size + process_resident + process_share) / process_elapsed_sec)
-
-    return process_mem_usage
-
-
-def plot_mem_usage(mem_usage_list, time_list):
-    with locking.lock:
-        plt.figure()
-        plt.plot(time_list, mem_usage_list)
-        plt.xlabel("Temps (s)")
-        plt.ylabel("Utilisation de la mémoire (%)")
-        plt.show()
-
-
-def store_mem_usage(mem_usage_list, time_list, pid):
-    with locking.lock:
-        plt.figure()
-        plt.plot(time_list, mem_usage_list)
-        plt.xlabel("Temps (s)")
-        plt.ylabel("Utilisation de la mémoire (%)")
-        plt.savefig(f"MEM_{pid}.png")
-        plt.close()
-
-
-def utilisation_mem(pid, frequence, nbre_points, result):
     process_info = Statm(pid)
     uptime_info = Uptime()
 
+    start = time.clock_gettime(time.CLOCK_REALTIME)
+    now = 0
+
     list_mem = []
     list_temps = []
-    compt = 0
-    while compt < nbre_points:
 
-        process_info.read_proc_statm()
-        uptime_info.read_proc_uptime()
+    while process_info.read_proc_statm() != -1 and uptime_info.read_proc_uptime() != -1 and now - start < interval:
+        now = time.clock_gettime(time.CLOCK_REALTIME)
 
         list_mem.append(process_info.size)
-        list_temps.append(compt*frequence)
-        compt += 1
+        list_temps.append(now - start)
 
-        time.sleep(frequence)
+        time.sleep(frequency / 60)
 
     result.append(Result("MEM",  "Utilisation mémoire (mB)", [list_temps, list_mem]))
+    return 0
     #store_mem_usage(list_mem, list_temps,pid)
