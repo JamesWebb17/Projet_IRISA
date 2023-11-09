f6449644b20cbe2a7f6fc52b22ec4b6c75e4d98a
Faucher Simon <sfaucher@enssat.fr>
2023-11-09 17:27:16 +0100

    Application des corrections + commentaire à POWER

--- Power/utilisation.py
+++ Power/utilisation.py
@@ -1,10 +1,25 @@
+""" @package Power
+Documentation for Power module.
+
+More details.
+Function for calculating the Power usage of a process.
+"""
+
 import time
 
 from Read_File import Uptime, Hwmon
 from shared import Result
 
 
-def utilisation_power(frequence, nbre_points, result):
+def utilisation_power(frequency, interval, result):
+    """
+    Find the Power usage of a process in files system.
+    :param frequency: point per second wanted
+    :param interval: interval of time wanted
+    :param result: array for sending the result to the main thread
+    :return: status of the function
+    """
+
     vdd_gpu_soc = Hwmon()
     vdd_gpu_soc.__set_name__("3", "1")
     vdd_cpu_cv = Hwmon()
@@ -16,6 +31,9 @@ def utilisation_power(frequence, nbre_points, result):
 
     uptime_info = Uptime()
 
+    start = time.clock_gettime(time.CLOCK_REALTIME)
+    now = 0
+
     list_power_vdd_gpu_soc = []
     list_power_vdd_cpu_cv = []
     list_power_vin_sys_5_v0 = []
@@ -23,28 +41,21 @@ def utilisation_power(frequence, nbre_points, result):
 
     list_temps = []
 
-    compt = 0
-    while compt < nbre_points:
-
-        vdd_gpu_soc.read("3", "1")
-        vdd_cpu_cv.read("3", "2")
-        vin_sys_5_v0.read("3", "3")
-        vddq_vdd2_1_v8_ao.read("4", "2")
-
-        uptime_info.read_proc_uptime()
+    while vdd_gpu_soc.read("3", "1") !=-1 and vdd_cpu_cv.read("3", "2") != -1 and vin_sys_5_v0.read("3", "3") != -1 and vddq_vdd2_1_v8_ao.read("4", "2") != -1 and uptime_info.read_proc_uptime() != -1 and now - start < interval:
+        now = time.clock_gettime(time.CLOCK_REALTIME)
 
         list_power_vdd_gpu_soc.append(vdd_gpu_soc.amps * vdd_gpu_soc.volts)
         list_power_vdd_cpu_cv.append(vdd_cpu_cv.amps * vdd_cpu_cv.volts)
         list_power_vin_sys_5_v0.append(vin_sys_5_v0.amps * vin_sys_5_v0.volts)
         list_power_vddq_vdd2_1_v8_ao.append(vddq_vdd2_1_v8_ao.amps * vddq_vdd2_1_v8_ao.volts)
 
-        list_temps.append(compt*frequence)
-        compt += 1
+        list_temps.append(now - start)
 
-        time.sleep(frequence)
+        time.sleep(frequency / 60)
 
     result.append(Result("POWER_"+vdd_gpu_soc.name, "Consomation énergétique (mW)", [list_temps, list_power_vdd_gpu_soc]))
     result.append(Result("POWER_"+vdd_cpu_cv.name, "Consomation énergétique (mW)", [list_temps, list_power_vdd_cpu_cv]))
     result.append(Result("POWER_"+vin_sys_5_v0.name, "Consomation énergétique (mW)", [list_temps, list_power_vin_sys_5_v0]))
     result.append(Result("POWER_"+vddq_vdd2_1_v8_ao.name, "Consomation énergétique (mW)", [list_temps, list_power_vddq_vdd2_1_v8_ao]))
+    return 0
     # store_mem_usage(list_mem, list_temps,pid)
