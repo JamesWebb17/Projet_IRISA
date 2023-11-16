5ee32f58944a10ee71abe7a3ef54bd16e644301c
Faucher Simon <sfaucher@enssat.fr>
2023-11-16 12:09:26 +0100

    test script 2

--- lancement.sh
+++ lancement.sh
@@ -8,18 +8,8 @@ julia_getpid="getpid()"
 julia_function_apprentissage="tup = f1_vs_epoch()"
 
 # Ouvrir le premier terminal pour Julia
-gnome-terminal --tab --bash "cd ~/Desktop/NOP_DeepLearning; juliadev -t auto -O3; julia -e 'println( getpid() )'" &
-
-# Attendre un court instant pour laisser le premier terminal s'ouvrir
-sleep 2
-
-# Récupérer le PID du processus Julia
-julia_pid=$(pgrep julia)
-
-# Afficher le PID
-echo "PID de Julia : $julia_pid"
-
-# Exécuter la fonction Julia
-echo "Exécution de la fonction Julia..."
+gnome-terminal --tab -- bash -c "cd ~/Desktop/NOP_DeepLearning; juliadev -t auto -O3; julia -e 'println( getpid() )'" &
+pid=$!
+echo "Le PID du processus Julia est : $pid"
 
 
