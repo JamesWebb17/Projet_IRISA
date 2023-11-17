901f47df1ee2d34227f5332fd89443e5ed39e409
Faucher Simon <sfaucher@enssat.fr>
2023-11-17 15:20:38 +0100

    Correction erreur

--- CPU/utilisation.py
+++ CPU/utilisation.py
@@ -95,10 +95,10 @@ def utilisation_cpus(frequency, interval, result):
         now = time.clock_gettime(time.CLOCK_REALTIME)
 
         process_info.cpu_stats.get("cpu").starttime = uptime_info.total_operational_time  # + uptime_info.idle_time
-        list_cpu.append(calcul_utilisation_cpu_systeme(process_info.cpu_stats.get("cpu"), uptime_info, 100))
-        for i in range(0, len(list_cpu)):
+        list_cpu[0].append(calcul_utilisation_cpu_systeme(process_info.cpu_stats.get("cpu"), uptime_info, 100))
+        for i in range(1, len(list_cpu)):
             process_info.cpu_stats.get(f"cpu{i}").starttime = uptime_info.total_operational_time #+ uptime_info.idle_time
-            list_cpu.append(calcul_utilisation_cpu_systeme(process_info.cpu_stats.get(f"cpu{i}"), uptime_info, 100))
+            list_cpu[i].append(calcul_utilisation_cpu_systeme(process_info.cpu_stats.get(f"cpu{i}"), uptime_info, 100))
 
         list_temps.append(now - start)
 
