45c62035da5ef4ef7c6ea56a23e8a5b7eca17c1b
Faucher Simon <sfaucher@enssat.fr>
2023-11-17 12:03:29 +0100

    test nouvelle façon de calculer % utilisation CPU 6

--- CPU/utilisation.py
+++ CPU/utilisation.py
@@ -64,20 +64,20 @@ def utilisation_cpu(pid, frequency, interval, result):
     return 0
 
 
-def calcul_utilisation_cpu_systeme(cpu, boot_time, clock_ticks_per_second):
-    total_time = cpu.utime + cpu.stime # en clock ticks
-    idle_time = cpu.idle[0] # en clock ticks
+def calcul_utilisation_cpu_systeme(cpu, uptime, clock_ticks_per_second):
+    total_time_moy = (cpu.utime + cpu.stime)/12 # en clock ticks
+    idle_time_moy = cpu.idle[0]/12 # en clock ticks
 
-    total_time_sec = total_time / clock_ticks_per_second
-    idle_time_sec = idle_time / clock_ticks_per_second
+    total_time_sec = total_time_moy / clock_ticks_per_second
+    idle_time_sec = idle_time_moy / clock_ticks_per_second
 
-    stattime_sec = boot_time / clock_ticks_per_second
+    stattime_sec = uptime.total_time
 
     print("stattime_sec : " + str(stattime_sec))
     print("idle_time_sec : " + str(idle_time_sec))
     print("total_time_sec : " + str(total_time_sec))
 
-    cpu_usage = 100 * (total_time_sec / boot_time)
+    cpu_usage = 100 * (total_time_sec / stattime_sec)
 
     #cpu_usage = 100 * (1 - ( stattime_sec - idle_time_sec) / total_time_sec)
 
@@ -104,7 +104,7 @@ def utilisation_cpus(frequency, interval, result):
     while process_info.read_stat() != -1 and uptime_info.read_proc_uptime() != -1 and now - start < interval:
         now = time.clock_gettime(time.CLOCK_REALTIME)
         process_info.cpu_stats.get("cpu").starttime = uptime_info.total_time #+ uptime_info.idle_time
-        list_cpu.append(calcul_utilisation_cpu_systeme(process_info.cpu_stats.get("cpu"), process_info.boot_time, 100))
+        list_cpu.append(calcul_utilisation_cpu_systeme(process_info.cpu_stats.get("cpu"), uptime_info, 100))
         list_temps.append(now - start)
 
         time.sleep(frequency / 60)
