30b1f6778a1d6f96b45abdde1dc1cbfcbe44dfdf
Faucher Simon <sfaucher@enssat.fr>
2023-11-17 14:33:49 +0100

    test autre méthode 2

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import psutil
 import time
 
-def calculate_cpu_usage():
-    # Utilisation du CPU depuis le dernier appel
-    cpu_percent = psutil.cpu_percent(interval=1)
+def calculate_cpu_usage(core):
+    # Utilisation du CPU pour le cœur spécifié depuis le dernier appel
+    cpu_percent = psutil.cpu_percent(interval=1, percpu=True)[core]
     return cpu_percent
 
 def main():
+    target_core = 0  # Remplacez cela par le numéro du cœur que vous souhaitez surveiller
+
     while True:
-        cpu_usage = calculate_cpu_usage()
-        print(f'CPU Usage: {cpu_usage}%')
+        cpu_usage = calculate_cpu_usage(target_core)
+        print(f'CPU Usage (Core {target_core}): {cpu_usage}%')
         time.sleep(1)
 
 if __name__ == "__main__":
